946cb7a7afcc9140ab5f8bc895b074c12dee6954
victorandujar <victorandujarmurcia@gmail.com>
2024-12-17 17:08:14 +0100

    Add locales config for translations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx,css}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        background: "var(--background-color)",
+        foreground: "var(--foreground)",
+        mainColor: "#888aff",
+        primary: "#6bcbb8",
+        secondary: "#005d63",
+        tertiary: "#ff8958",
+      },
+      fontSize: {
+        xxl: "var(--font-XXL)",
+        xl: "var(--font-XL)",
+        l: "var(--font-L)",
+        m: "var(--font-M)",
+        s: "var(--font-S)",
+        ms: "var(--font-MS)",
+        mss: "var(--font-MSS)",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
