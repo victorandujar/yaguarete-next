feb61802d27e777cc691c15d200e611ff8ba702b
victorandujar <victorandujarmurcia@gmail.com>
2024-12-18 17:38:27 +0100

    Modify paddings for all components

--- src/app/components/WelcomeSection/WelcomeSection.tsx
+++ src/app/components/WelcomeSection/WelcomeSection.tsx
@@ -2,17 +2,17 @@ import { useTranslations } from "next-intl";
 import Image from "next/image";
 
 const WelcomeSection = (): React.ReactElement => {
-  const t = useTranslations("AboutSection");
+  const t = useTranslations("WelcomeSection");
   return (
     <section className="border-b border-black w-full flex h-fit">
-      <div className="px-40 py-20 w-2/3 border-r border-black flex flex-col">
+      <div className="px-80 py-20 w-2/3 border-r border-black flex flex-col">
         <h2 className="font-ppHatton text-black text-xxl tracking-[2px] p-0 m-0">
           {t("title")} YAGUARETÉ
         </h2>
         <span className="font-ppHatton text-black text-l">
           ESOTERIC & SUSTAINABLE CAFÉ
         </span>
-        <section className="flex flex-col gap-10 text-black w-3/5 pt-16 text-m">
+        <section className="flex flex-col gap-10 text-black w-4/5 pt-16 text-m">
           <span>{t("top-text")}</span>
           <span>{t("bottom-text")}</span>
         </section>
@@ -24,7 +24,7 @@ const WelcomeSection = (): React.ReactElement => {
       <div className="w-1/3 flex justify-center p-10">
         <div className="w-full h-full bg-white p-2 relative">
           <Image
-            src={"/jungle.jpg"}
+            src={"/images/jungle.jpg"}
             alt="About photo. Jungle leaves"
             layout="fill"
             className="object-cover rounded-lg"
