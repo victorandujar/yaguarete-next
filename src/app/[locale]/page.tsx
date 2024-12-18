d78f0846ac37df0f3f0d6d31d17f67eda7742d70
victorandujar <victorandujarmurcia@gmail.com>
2024-12-18 17:39:33 +0100

    Add AboutSection to home + modify public images structure

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,13 @@
 import WelcomeSection from "../components/WelcomeSection/WelcomeSection";
 import HeroSection from "../components/HeroSection/HeroSection";
+import AboutSection from "../components/AboutSection/AboutSection";
 
 export default function Home() {
   return (
     <main className="flex flex-col">
       <HeroSection />
       <WelcomeSection />
+      <AboutSection />
     </main>
   );
 }
