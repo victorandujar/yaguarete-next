75025f1ea904daae80840494271ba1ee7550e529
victorandujar <victorandujarmurcia@gmail.com>
2024-12-18 10:27:44 +0100

    Add Header component + add its functionality & styles

---
+++ src/app/components/Header/utils/headerSections.ts
@@ -0,0 +1,19 @@
+import { HeaderSection } from "@/interfaces/Header";
+
+export const headerSections: HeaderSection[] = [
+  {
+    id: 1,
+    name: "events",
+    link: "",
+  },
+  // {
+  //   id: 2,
+  //   name: "shop",
+  //   link: "",
+  // },
+  {
+    id: 3,
+    name: "menu",
+    link: "",
+  },
+];
