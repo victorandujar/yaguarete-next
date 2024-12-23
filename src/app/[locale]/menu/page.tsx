70e5f37041538bb604b132c39fd3c14cf9752e50
victorandujar <victorandujarmurcia@gmail.com>
2024-12-23 17:13:49 +0100

    Add menu download section + its functionality + styles

--- src/app/[locale]/menu/page.tsx
+++ src/app/[locale]/menu/page.tsx
@@ -1,3 +1,5 @@
+import MenuDownloadSection from "@/app/components/MenuCompos/MenuDownloadSection/MenuDownloadSection";
+import menuTexts from "@/app/components/MenuCompos/MenuDownloadSection/utils/menuTexts";
 import MenuWelcomeSection from "@/app/components/MenuCompos/MenuWelcomeSection/MenuWelcomeSection";
 import TextWithIconSection from "@/app/components/MenuCompos/TextWithIconSection/TextWithIconSection";
 
@@ -6,6 +8,20 @@ const MenuPage = (): React.ReactElement => {
     <main className="h-full w-screen text-black py-20">
       <MenuWelcomeSection />
       <TextWithIconSection />
+      <div className="flex justify-center gap-32 py-32 px-72">
+        <MenuDownloadSection
+          color={"primary"}
+          title="Menú"
+          download_text={menuTexts.es}
+          menu_file="/menus/MENÚESPANOLCOMPLETO.pdf"
+        />
+        <MenuDownloadSection
+          color={"secondary"}
+          title="Menu"
+          download_text={menuTexts.en}
+          menu_file="/menus/MENÚINGLÉSCOMPLETO.pdf"
+        />
+      </div>
     </main>
   );
 };
---
+++ src/app/components/MenuCompos/MenuDownloadSection/utils/menuTexts.ts
@@ -0,0 +1,6 @@
+const menuTexts = {
+  es: "Click aquí para descargar el menú",
+  en: "Click here to download the menu",
+};
+
+export default menuTexts;
